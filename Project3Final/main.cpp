def03b0b91b2c75db7cf39553ae34b78d131d596
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-08-05 18:45:28 -0400

    Update main.cpp
    Updated menu

--- Project3Final/main.cpp
+++ Project3Final/main.cpp
@@ -20,65 +20,76 @@ int main() {
 
     while (true)
     {
-        cout << "Menu:" << endl;
-        cout << "1. BFS" << endl;
-        cout << "2. Dijkstra's" << endl;
-        cout << "3. Redo" << endl;
-        cout << "4. Check if two actors share a movie" << endl;
-        cout << "5. Exit" << endl;
+        cout << "o - - - - - - - - - - - - - - - - - - - o " << endl;
+        cout << "| ------------------------------------- |" << endl;
+        cout << "|  Menu:                                |" << endl;
+        cout << "|                                       |" << endl;
+        cout << "|  1. BFS                               |" << endl;
+        cout << "|  2. Dijkstra's                        |" << endl;
+        cout << "|  3. Redo                              |" << endl;
+        cout << "|  4. Check if two actors share a movie |" << endl;
+        cout << "|  5. Exit                              |" << endl;
+        cout << "| ------------------------------------- |" << endl;
+        cout << "o - - - - - - - - - - - - - - - - - - - o" << endl;
 
-        cout << "Enter 1, 2, 3, or 4 to proceed" << endl;
+        cout << endl;
+
+        cout << "Please enter 1, 2, 3, 4, or 5 to proceed: ";
         cin >> choice;
 
         cin.ignore();
 
-        if (choice == 1)
-        {
+        cout << endl;
+
+        if (choice == 1) {
             //User input first/second actor
             cout << "Starting actor: ";
             getline(cin, startActor);
             cout << "Ending actor: ";
             getline(cin, endActor);
+
+            cout << endl;
 
             //Call BFS method
             m.BFS(startActor, endActor);
         }
-        else if (choice == 2)
-        {
+        else if (choice == 2) {
             //User input first/second actor
             cout << "Starting actor: ";
             getline(cin, startActor);
             cout << "Ending actor: ";
             getline(cin, endActor);
 
+            cout << endl;
+
             //Call dijkstras? Not sure what to call this
             m.dijkstras(startActor, endActor);
         }
-        else if (choice == 3)
-        {
+        else if (choice == 3) {
             //Redo the loop, continue
             continue;
         }
 
         else if(choice == 4) {
+
             cout << "Check if actors share a movie" << endl;
             getline(cin, startActor);
             getline(cin, endActor);
             bool check;
             check = m.ShareMovies(startActor, endActor);
-            if(check)
+
+            if(check) {
                 cout << "They do share a movie" << endl;
+            }
             else if(!check) {
                 cout << "They do not share a movie" << endl;
             }
         }
-        else if (choice == 5)
-        {
+        else if (choice == 5) {
             //Exit the program
             return 0;
         }
-        else
-        {
+        else {
             cout << "Invalid input" << endl;
         }
     }
