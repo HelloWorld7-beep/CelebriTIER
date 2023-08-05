e34201f9c8cf8b28a2ef94a67cbb4a8d74c1e39a
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2023-08-04 21:30:01 -0400

    Update actors.cpp

--- actors.cpp
+++ actors.cpp
@@ -71,3 +71,82 @@ unordered_map<int,vector<string>> actors::FilterActorsHelper() {
 
     return actorMovies;
 }
+
+//BFS method
+void actors::BFS(const string& start, const string& end)
+{
+    //If the two actors are the same
+    if (start == end)
+    {
+        //Print message
+        cout << "Please pick two different actors" << endl;
+        //Return
+        return;
+    }
+
+    //BFS traversal path queue
+    queue<vector<string>> queue;
+    //Set keeping track of actors visited in path, unordered because don't need order
+    unordered_set<string> visitedActors;
+
+    //Start finding the path by enqueueing the start actor
+    queue.push({start});
+    visitedActors.insert(start);
+
+    //Iterate through BFS
+    while (!queue.empty())
+    {
+        //Dequeue first element, and pop
+        vector<string> path = queue.front();
+        queue.pop();
+
+        //Last element of the current path
+        string currActor = path.back();
+
+        cout << "Current actor: " << currActor << endl;
+
+        //Find neighbor of the current vector from the adjacencyList
+        vector<pair<string, string>> neighbors = adjList[currActor];
+
+        for (const auto& neighbor : neighbors)
+        {
+            const string& nextActor = neighbor.second;
+            //Checking if next actor has been visited yet
+            if (visitedActors.find(nextActor) == visitedActors.end())
+            {
+                //Inserting next actor into visited since we're visiting it
+                visitedActors.insert(nextActor);
+
+                cout << "Visiting neighbor: " << nextActor << endl;
+
+                //Create a new path
+                vector<string> currPath = path;
+
+                //Add to path
+                currPath.push_back(nextActor);
+                currPath.push_back(neighbor.first);
+
+                //If the actor is reached
+                if (nextActor == end)
+                {
+                    cout << "Path found between " << start << " and " << end << ": ";
+
+                    //Print the movies, every other element is a movie
+                    for (int i = 1; i < currPath.size(); i += 2)
+                    {
+                        cout << currPath[i] << " -> ";
+                    }
+                    cout << endl;
+                    //Return
+                    return;
+                }
+
+                //If not reached the end yet, push the current path onto the queue
+                queue.push(currPath);
+            }
+        }
+    }
+
+    //If no path is found, print a message
+    cout << "No path found between " << start << " and " << end << endl;
+}
